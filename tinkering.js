06b3610458ea3bb6cf23f64dc1018135a02963d6
Kurt Galvin <kurtgalvin@gmail.com>
2020-02-17 18:34:46 +0000

    howManyHundreds function

--- tinkering.js
+++ tinkering.js
@@ -1,1 +1,9 @@
-console.log(4096 % 12);
+function howManyHundreds(n) {
+  return Math.round((n/100) - 0.5)
+}
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
